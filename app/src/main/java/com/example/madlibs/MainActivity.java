e5f8a493beaffe0cb58cefeddcc209dcbff34b90
201101583 <201101583@inv515563.lbusd.local>
2023-05-23 11:53:57 -0700

    Updated may 23rd

--- app/src/main/java/com/example/madlibs/MainActivity.java
+++ app/src/main/java/com/example/madlibs/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.madlibs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //all buttons
+    private Button btnRoar;
+
+    //all media players
+    private MediaPlayer mpRoar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //buttons
+        btnRoar = findViewById(R.id.btn_roar);
+
+
+        //media players
+        mpRoar = MediaPlayer.create(this,R.raw.sound_roar);
+
+
+        btnRoar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mpRoar.start();
+            }
+        });
     }
 }
